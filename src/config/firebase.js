67fbd3d748858c3716252ae1c1e06c848b1b3096
LeeLa Bhaskar <107635790+leelabhaskar22@users.noreply.github.com>
2025-04-05 11:51:28 +0530

    Update firebase.js
    config file updated

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth"
 import {getFirestore}  from "firebase/firestore"
 const firebaseConfig = {
-  apiKey: "AIzaSyAdzq1rEAEhZ8-2IH6OWs6kCNwip6Od8WM",
-  authDomain: "test-4351b.firebaseapp.com",
-  projectId: "test-4351b",
-  storageBucket: "test-4351b.appspot.com",
-  messagingSenderId: "1093523192578",
-  appId: "1:1093523192578:web:c04cbb4c41eda13002601b"
+  apiKey: "your key",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
